5cb09f95c86c086a74d1d23ea3330ddb4f59b739
Arpit Gupta <arpitguptafzd123@gmail.com>
2025-12-18 12:41:55 +0530

    [Arpit Gupta] Power of a no raised to exponent calculated successfully

---
+++ core-java-practice/gcr-codebase/programming-elements/power_of_a_no.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class power_of_a_no {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int base = 10;
+		int exponent = 3;
+		System.out.println(Math.pow(base, exponent));
+	}
+
+}
