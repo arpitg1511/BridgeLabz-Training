25120f41851c6db51223230a0e67f636c679112b
Arpit Gupta <arpitguptafzd123@gmail.com>
2025-12-18 12:29:15 +0530

    [Arpit Gupta] Simple Interest calculated sucesfully

---
+++ core-java-practice/gcr-codebase/programming-elements/simple_interest.java
@@ -0,0 +1,9 @@
+
+public class simple_interest {
+	public static void main(String[] args) {
+		int princ = 1000;
+		int rate = 10;
+		int time = 2;
+		System.out.println((princ * rate * time) / 100);
+	}
+}
