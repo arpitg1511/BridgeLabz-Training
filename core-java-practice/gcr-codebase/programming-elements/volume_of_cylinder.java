74232fbedf8b6a028cad1809a74b201195c91a47
Arpit Gupta <arpitguptafzd123@gmail.com>
2025-12-18 11:55:32 +0530

    [Arpit Gupta] Volume of Cylinder calculatd succesfully

---
+++ core-java-practice/gcr-codebase/programming-elements/volume_of_cylinder.java
@@ -0,0 +1,12 @@
+
+public class volume_of_cylinder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int r = 12;
+		int h = 5;
+		double ans = Math.PI * r * r * h;
+		System.out.printf("%.2f", ans);
+	}
+
+}
