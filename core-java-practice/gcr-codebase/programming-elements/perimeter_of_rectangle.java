fc8b42f69bd52bdcd37aeb1c1c8508e42b877a57
Arpit Gupta <arpitguptafzd123@gmail.com>
2025-12-18 12:19:05 +0530

    [Arpit Gupta] Permieter of rectangle calculated sucesfully

---
+++ core-java-practice/gcr-codebase/programming-elements/perimeter_of_rectangle.java
@@ -0,0 +1,11 @@
+
+public class perimeter_of_rectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int length = 10;
+		int breadth = 20;
+		System.out.println(2*(length + breadth));
+	}
+
+}
