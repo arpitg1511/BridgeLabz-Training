ac05989541c2d645e93c2a12b8d4d440ef3fc623
Arpit Gupta <arpitguptafzd123@gmail.com>
2025-12-18 11:35:16 +0530

    Celsius to Fahrenheit

---
+++ core-java-practice/gcr-codebase/programming-elements/celsius_to_fahrenheit.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class celsius_to_fahrenheit {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int temp = sc.nextInt();
+		double ans = (temp * 1.8) + 32;
+		System.out.println(ans);
+	}
+
+}
