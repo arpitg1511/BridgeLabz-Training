bcf1831f739f46c12870314ae1e55a19383033d8
Arpit Gupta <arpitguptafzd123@gmail.com>
2025-12-18 15:27:23 +0530

    [Arpit Gupta] Age of Harry Calculated Successfully

--- core-java-practice/gcr-codebase/programming-elements/AgeOfHarry.java
+++ core-java-practice/gcr-codebase/programming-elements/AgeOfHarry.java
@@ -3,6 +3,6 @@ public class AgeOfHarry {
 	public static void main(String[] args) {
 		int cur_year = 2024;
 		int birth_year = 2000;
-		System.out.println(cur_year - birth_year);
+		System.out.println("Harry's age in 2024 is " + (cur_year - birth_year));
 	}
 }
