3c84f5b1c26bfad47780e2aa5a875db51ac487db
Arpit Gupta <arpitguptafzd123@gmail.com>
2025-12-20 00:27:24 +0530

    [Arpit Gupta] Refactor . Correct name added

--- core-java-practice/gcr-codebase/control-flow/CountDownForLaunchWhileLoop.java
+++ core-java-practice/gcr-codebase/control-flow/CountDownForLaunchWhileLoop.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class CountDownForLaunch {
+public class CountDownForLaunchWhileLoop {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
