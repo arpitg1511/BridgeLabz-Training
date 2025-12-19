e5726cf464a44fefe4216038e833d17c84737037
Arpit Gupta <arpitguptafzd123@gmail.com>
2025-12-20 00:43:44 +0530

    [Arpit Gupta] Refactor . Solved Running Sum With Break statement Successfully

--- core-java-practice/gcr-codebase/control-flow/RunningSumWithoutNegatives.java
+++ core-java-practice/gcr-codebase/control-flow/RunningSumWithoutNegatives.java
@@ -1,15 +1,25 @@
 import java.util.*;
+
 public class RunningSumWithoutNegatives {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		double sum = 0.0;
-		double a = sc.nextDouble(); //Input first number
-		while(a <= 0) { //Check for zero or negative input
-			sum += a;
-			a = sc.nextDouble(); // Input next number
-		}
-		
-		System.out.println(sum);
-		sc.close();
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double sum = 0.0;
+
+        // Infinite loop to keep taking input until stop condition is met
+        while (true) {
+            double num = sc.nextDouble();
+
+            // If user enters 0 or a negative number, stop taking input
+            if (num <= 0) {
+                break;
+            }
+
+            // Add only positive numbers to the sum
+            sum += num;
+        }
+
+        System.out.println(sum);
+        sc.close();
+    }
 }
