e29e022a8e7d8e8792390b266727cb4e9dfed6a7
Arpit Gupta <arpitguptafzd123@gmail.com>
2026-01-08 12:41:36 +0530

    [Arpit Gupta] Add . Missing number solved successfully

---
+++ core-java-practice/leet-code-codebase/MissingNumber.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            xor ^= i ^ nums[i];
+        }
+
+        xor ^= nums.length;
+
+        return xor;
+    }
+}
