dd3c717c427597d10446d6e8dc10cd9f79cd4afd
Arpit Gupta <arpitguptafzd123@gmail.com>
2026-01-19 14:47:52 +0530

    [Arpit Gupta] Add . Use Case III performed successfully

--- reviews/address-book/AddressBook.java
+++ reviews/address-book/AddressBook.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class AddressBook {
 	public static void main(String[] args) {
 		Contact c1 = new Contact("Arpit", "Gupta", "Firozabad", "Firozabad", "283203", "7505XXXXXX", "xyz@gmail.com");
@@ -8,6 +8,7 @@ public class AddressBook {
 		Contact c2 = new Contact("Aman", "Gupta", "Ghaziabad", "Firozabad", "281203", "7105XXXXXX", "xqz@gmail.com");
 
 		
-		AddressBookService.addContact(c2);
+//		AddressBookService.addContact(c2);
+		AddressBookService.editContactByName("Arpit", c2);
 	}
 }
--- reviews/address-book/AddressBookService.java
+++ reviews/address-book/AddressBookService.java
@@ -1,14 +1,34 @@
 import java.util.*;
 public class AddressBookService {
 	public static void addContact(Contact c) {
-		if(!AddressBookData.contactMap.containsKey(c.firstName)) {
+		if(!AddressBookData.contactMap.containsKey(c.firstName.toLowerCase())) {
 			AddressBookData.contactList.add(c);
-			AddressBookData.contactMap.put(c.firstName, c);
+			AddressBookData.contactMap.put(c.firstName.toLowerCase(), c);
 			System.out.println("Contact added successfully");
 		}
 		
 		else {
 			System.out.println("Contact with same Number already exists");
+		}
+	}
+	
+	public static void editContactByName(String name, Contact c) {
+		if(!AddressBookData.contactMap.containsKey(name.toLowerCase())) {
+			System.out.println("Contact does not exist");
+		}
+		
+		else {
+			Contact newContact = c;
+			AddressBookData.contactMap.remove(name.toLowerCase());
+			for(int i = 0; i < AddressBookData.contactList.size(); i++) {
+				Contact contact = AddressBookData.contactList.get(i);
+				if(contact.firstName.toLowerCase().equals(name)) {
+					AddressBookData.contactList.remove(i);
+				}
+			}
+			addContact(newContact);
+			
+			System.out.println("Contact edited successfuly");
 		}
 	}
 }
