ef9a3d2962dc65fd2f537478f0b13a1c5d4d3c1e
Arpit Gupta <arpitguptafzd123@gmail.com>
2026-01-19 15:01:29 +0530

    [Arpit Gupta] Use Case IV performed successfully

--- reviews/address-book/AddressBook.java
+++ reviews/address-book/AddressBook.java
@@ -10,5 +10,6 @@ public class AddressBook {
 		
 //		AddressBookService.addContact(c2);
 		AddressBookService.editContactByName("Arpit", c2);
+		AddressBookService.deleteContactByName("Aman");
 	}
 }
--- reviews/address-book/AddressBookService.java
+++ reviews/address-book/AddressBookService.java
@@ -1,5 +1,8 @@
 import java.util.*;
 public class AddressBookService {
+	
+	// Use Case 1
+	// Use Case 2
 	public static void addContact(Contact c) {
 		if(!AddressBookData.contactMap.containsKey(c.firstName.toLowerCase())) {
 			AddressBookData.contactList.add(c);
@@ -12,6 +15,7 @@ public class AddressBookService {
 		}
 	}
 	
+	// Use Case 3
 	public static void editContactByName(String name, Contact c) {
 		if(!AddressBookData.contactMap.containsKey(name.toLowerCase())) {
 			System.out.println("Contact does not exist");
@@ -26,9 +30,29 @@ public class AddressBookService {
 					AddressBookData.contactList.remove(i);
 				}
 			}
+			
 			addContact(newContact);
 			
 			System.out.println("Contact edited successfuly");
+		}
+	}
+	
+	//Use Case 4
+	public static void deleteContactByName(String name) {
+		if(!AddressBookData.contactMap.containsKey(name.toLowerCase())) {
+			System.out.println("Contact does not exist");
+		}
+		
+		else {
+			AddressBookData.contactMap.remove(name.toLowerCase());
+			for(int i = 0; i < AddressBookData.contactList.size(); i++) {
+				Contact contact = AddressBookData.contactList.get(i);
+				if(contact.firstName.toLowerCase().equals(name)) {
+					AddressBookData.contactList.remove(i);
+				}
+			}
+			
+			System.out.println("Contact Deleted Successfully");
 		}
 	}
 }
